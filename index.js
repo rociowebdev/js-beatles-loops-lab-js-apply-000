fa714a637734890200194889e1cf039e7fba44f3
considerate-inspector-9255 <rociocordero@gmail.com>
2020-03-15 23:08:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function theBeatlesPlay(musicians, instruments) {
    musicians = [ ];
    instruments = [ ];
 }
+
+function howMany(selectObject) {
+  let numberSelected = 0;
+  for (let i = 0; i < selectObject.options.length; i++) {
+    if (selectObject.options[i].selected) {
+      numberSelected++;
+    }
+  }
+  return numberSelected;
+}
